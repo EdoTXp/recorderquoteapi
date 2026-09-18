fb82350971d122b9bedfa33dfcb7442069cf86ff
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2026-09-18 16:57:10 -0300

    first commit

---
+++ src/main/java/com/deiovannagroup/recorderquoteapi/RecorderquoteapiApplication.java
@@ -0,0 +1,13 @@
+package com.deiovannagroup.recorderquoteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecorderquoteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecorderquoteapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deiovannagroup/recorderquoteapi/RecorderquoteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deiovannagroup.recorderquoteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecorderquoteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
